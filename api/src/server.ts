2af0306242c06acfa0a83d4a8eba361d215d1352
Anthony Gatti <amgatti4@gmail.com>
2024-07-10 14:17:34 -0400

    Added routes and route validation

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,13 @@
 import http from 'http';
 import express from 'express';
+import 'reflect-metadata';
+
 import { loggingHandler } from './middleware/loggingHandler';
 import { corsHandler } from './middleware/corsHandler';
 import { routeNotFound } from './middleware/routeNotFound';
 import { SERVER, SERVER_HOSTNAME, SERVER_PORT } from './config/config';
+import MainController from './controllers/main';
+import { defineRoutes } from './modules/routes';
 
 
 export const application = express();
@@ -25,9 +29,7 @@ export const Main = () => {
     console.info('------------------------------------------');
     console.info('Define Controller Routing');
     console.info('------------------------------------------');
-    application.get('/main/healthcheck', (req, res, next) => {
-        return res.status(200).json({ hello: 'world!'});
-    });
+    defineRoutes([MainController], application);
 
     console.info('------------------------------------------');
     console.info('Define Controller Routing');
