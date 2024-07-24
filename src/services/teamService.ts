298901027d1bef0ca5cfd0ded42d9c9a8004abd7
Anthony Gatti <amgatti4@gmail.com>
2024-07-24 17:27:04 -0400

    getTeamPoints making a query to notion but blocked by CORS

--- src/services/teamService.ts
+++ src/services/teamService.ts
@@ -1,15 +1,35 @@
 import axios from 'axios';
+import { Client } from '@notionhq/client';
+import { PageObjectResponse, PartialPageObjectResponse } from '@notionhq/client/build/src/api-endpoints';
+const notion = new Client({ auth: 'secret_fbrN9wJ2EnLQkLv7K9u74Sn2jYlyRsmT7EoZfhpSpem' });
 
 const API_URL = 'http://localhost:1337/team';
 
+function isFullPageObjectResponse(
+  page: PageObjectResponse | PartialPageObjectResponse
+): page is PageObjectResponse {
+  return 'properties' in page;
+}
+
 export const getTeamPoints = async (number: number) => {
   try {
-    const response = await axios.get(API_URL, {
-      params: {
-        number: number
-      }
+    console.log("CALL MADE HERE");
+    const response = await notion.databases.query({
+      database_id: 'c84b190cdbaf4e2a8683a9229e51696a?v=6ed5745825a641ed9b09007d8756ab2e',
+      filter: {
+        property: 'Name',
+        title: {
+          equals: 'Team ' + number,
+        },
+      },
     });
-    return response.data;
+    console.log("RESPONSE: ", response);
+
+    
+    const points = {total_points: number}
+    points.total_points = 0;
+
+    return points;
   } catch (error) {
     console.error('Error fetching points:', error);
     throw error;
