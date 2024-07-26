e37d4afcfa2e01dd826404d5403994389d5c8e46
Anthony Gatti <amgatti4@gmail.com>
2024-07-26 13:20:30 -0400

    removed api connected to postgres. renamed notion proxy to api

--- api/build/middleware/routeNotFound.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routeNotFound = routeNotFound;
-function routeNotFound(req, res, next) {
-    const error = new Error('Route Not Found');
-    console.error(error);
-    return res.status(404).json({ error: error.message });
-}
--- api/src/middleware/routeNotFound.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export function routeNotFound(req: Request, res: Response, next: NextFunction) {
-    const error = new Error('Route Not Found');
-
-    console.error(error)
-
-    return res.status(404).json({ error: error.message });
-}
